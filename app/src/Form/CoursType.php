3b59e4e2d39a1c8ca839089fbfc9274b45468f41
cedricdsst <cedricdsst@gmail.com>
2024-03-08 00:54:47 +0100

    association des classes_cours

--- app/src/Form/CoursType.php
+++ app/src/Form/CoursType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
+
 class CoursType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -38,10 +39,21 @@ class CoursType extends AbstractType
                 ],
                 'placeholder' => '--Sélectionnez une catégorie--',
                 'attr' => ['class' => 'block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'],
+                
             ])
             ->add('id_formateur', EntityType::class, [
                 'class' => Formateur::class,
                 'choice_label' => 'id',
+            ])
+            ->add('id_classe', EntityType::class, [
+                'class' => Classe::class,
+                'choice_label' => 'titre',
+                'multiple' => true, // permet la sélection multiple
+                'expanded' => false, // pour une sélection de type 'select', mettre à true pour des checkboxes
+                'label' => 'Classes',
+                'attr' => ['class' => 'your-custom-class-for-select'],
+                'label_attr' => ['class' => 'your-custom-class-for-label'],
+                'placeholder' => '--Sélectionnez les classes--'
             ])
             ->add('Ajouter', SubmitType::class)
 
