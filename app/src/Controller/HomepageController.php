55d0040391ec663f7d1402d052ca4be9579a7f3d
BrahimBoutagjat <brahimboutagjat@gmail.com>
2024-03-07 15:07:07 +0100

    Ajout champs manquants AddCours

--- app/src/Controller/HomepageController.php
+++ app/src/Controller/HomepageController.php
@@ -51,7 +51,7 @@ class HomepageController extends AbstractController
     #[Route('/addEleves', name: 'app_addEleves')]
     public function addeleves(): Response
     {
-        return $this->render('dashboards/AddElevesFormateur.html.twig', [
+        return $this->render('dashboards/AddEtudiant.html.twig', [
             'controller_addeleves' => 'AddElevesController',
         ]);
     }
@@ -77,7 +77,7 @@ class HomepageController extends AbstractController
     {
         // $questionCount = $request->query->get('question_count', 1); // Default to 1 question
 
-        
+
         return $this->render('dashboards/AddCours.html.twig', [
             'controller_AddCours' => 'HomepageController',
             // 'question_count' => $questionCount,
