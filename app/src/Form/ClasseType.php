e40b364328bf9eb54bfab157d2db133de576f016
lorada.andre <lorada.andre@omerin.com>
2024-03-08 09:04:20 +0100

    fin pour organisme et classe

--- app/src/Form/ClasseType.php
+++ app/src/Form/ClasseType.php
@@ -9,6 +9,7 @@ use App\Entity\Organisme;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,18 +18,31 @@ class ClasseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('id_formation', EntityType::class, [
-                'class' => Formation::class,
-                'choice_label' => 'id',
-            ])
+            ->add('titre', TextType::class, [
+                'attr' => [
+                    'class' => 'mb-4 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline',
+                    'placeholder' => 'IW2'
+                ],
+                'label' => 'Nom de la classe'
+                ])
             ->add('id_organisme', EntityType::class, [
                 'class' => Organisme::class,
                 'choice_label' => 'nom',
+                'label' => 'Organisme de formation',
+                'placeholder' => '-- Sélectionnez un organisme --',
+                'attr' => ['class' => 'mb-4 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'],
             ])
-            ->add('Ajouter', SubmitType::class)
-            
-        ;
+            ->add('id_formation', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'intitule',
+                'label' => 'Formation',
+                 'placeholder' => '-- Sélectionnez une formation --',
+                'attr' => ['class' => 'mb-8 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'],
+
+            ])
+            ->add('Ajouter', SubmitType::class, [
+                'attr' => ['class' => 'bg-orange-500 text-white p-2 rounded-md w-full']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- app/src/Form/OrganismeType.php
+++ app/src/Form/OrganismeType.php
@@ -2,10 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Formateur;
 use App\Entity\Organisme;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -16,10 +15,30 @@ class OrganismeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('adresse')
-            ->add('ville')
-            ->add('Ajouter', SubmitType::class)
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'mb-4 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline',
+                    'placeholder' => 'ESGI Lyon'
+                ],
+                'label' => 'Nom de l\'organisme'
+            ])
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'mb-4 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline',
+                    'placeholder' => 'Adresse'
+                ],
+                'label' => 'Nom de l\'organisme'
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'mb-4 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline',
+                    'placeholder' => 'Lyon'
+                ],
+                'label' => 'Ville'
+            ])
+            ->add('Ajouter', SubmitType::class, [
+                'attr' => ['class' => 'bg-orange-500 text-white p-2 rounded-md w-full']
+            ]);
             
         ;
     }
